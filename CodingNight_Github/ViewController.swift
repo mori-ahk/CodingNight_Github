fdc5a9f2f3f322766d53a6a49b0491de552d6c34
Morteza <morinob93@gmail.com>
2019-02-15 22:01:21 -0500

    Initial Commit

---
+++ CodingNight_Github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingNight_Github
+//
+//  Created by Morteza Ahmadi on 2019-02-15.
+//  Copyright © 2019 Morteza Ahmadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
